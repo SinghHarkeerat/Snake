af42bbbd4666d8744188499a6625d1e9cf25d79d
Harkeerat Singh <singhharkeerat110@gmail.com>
2024-11-17 01:49:16 -0800

    grid draws now

---
+++ main.py
@@ -0,0 +1,35 @@
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((500, 500))
+
+pygame.display.set_caption("Snake")
+
+# def drawGrid():
+#     for i in range(10):
+
+def drawGrid():
+    for i in range(10):
+        for j in range(10):
+            pygame.draw.rect(screen, (255, 255, 255), pygame.Rect(i *50, j*50, 50, 50), 1)
+            pygame.display.update()
+        pygame.display.update()
+    pygame.display.update()
+
+def snake():
+    size = 1
+    
+
+        
+
+while True:
+    drawGrid()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+            break
+    pygame.display.update()
+
+        
