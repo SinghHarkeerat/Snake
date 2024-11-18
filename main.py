6483587d2d155f9c79bac8f329a307601e7c2b48
Harkeerat Singh <singhharkeerat110@gmail.com>
2024-11-17 21:11:04 -0800

    a

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 import pygame
+import time
 
 pygame.init()
 
 screen = pygame.display.set_mode((500, 500))
 
 pygame.display.set_caption("Snake")
-
-# def drawGrid():
-#     for i in range(10):
 
 def drawGrid():
     for i in range(10):
@@ -17,14 +15,21 @@ def drawGrid():
         pygame.display.update()
     pygame.display.update()
 
-def snake():
-    size = 1
+def drawSnake():
+    for i in range(10):
+        for j in range(10):
+            pygame.draw.rect(screen, (0, 255, 0), pygame.)
     
+        
+    
+    
+
 
         
 
 while True:
-    drawGrid()
+    # drawGrid()
+    moveCube()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
